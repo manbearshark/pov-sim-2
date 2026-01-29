23f32b5ed0412ba6fe3e92acdbc6df332f7ce086
manbearshark <igorski@amazon.com>
2026-01-28 15:21:20 -1000

    flights services instrumented fully

--- flights/app.py
+++ flights/app.py
@@ -2,6 +2,24 @@ from flasgger import Swagger
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from utils import get_random_int
+import logging
+from uuid import uuid4
+from opentelemetry import trace
+from opentelemetry import metrics
+from random import randint
+
+# Acquire a tracer
+tracer = trace.get_tracer("flights.tracer")
+# Acquire a meter.
+meter = metrics.get_meter("flights.meter")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+flight_check_counter = meter.create_counter(
+    "flights.checks",
+    description="The number of calls to the /flights endpoint",
+)
 
 app = Flask(__name__)
 Swagger(app)
@@ -46,10 +64,21 @@ def get_flights(airline):
       200:
         description: Returns a list of flights for the selected airline
     """
+
     status_code = request.args.get("raise")
     if status_code:
       raise Exception(f"Encountered {status_code} error") # pylint: disable=broad-exception-raised
     random_int = get_random_int(100, 999)
+
+    with tracer.start_as_current_span("flights", kind=trace.SpanKind.SERVER) as flight_span:
+      # trace span
+      flight_span.set_attribute("airline", airline)
+      # metric
+      flight_check_counter.add(1, {"airline": 1})
+      # log
+      logmsg = "flights checked for airline: " + airline
+      logger.info(logmsg)
+
     return jsonify({airline: [random_int]}), 200
 
 @app.route("/flight", methods=["POST"])
